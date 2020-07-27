53dde37f60066e0c33dbaa12d8abb26aad3df838
sandro gagoshidze <49387424+sannyybg@users.noreply.github.com>
2020-07-27 16:25:58 +0400

    Add files via upload

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,54 @@
+﻿using muscshop.Context;
+using muscshop.filters;
+using muscshop.Helper;
+using muscshop.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace muscshop.Controllers
+{
+    [SessionFilter]
+    public class ShoppingCartController : Controller
+    {
+        private StoreContext _storeContext = new StoreContext();
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            var cartViewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+            
+
+            return View(cartViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(int id)
+        {
+            var album = _storeContext.Albums.Single(x => x.AlbumId == id);
+                
+            var currentcart = ShoppingCart.GetCart(this.HttpContext);
+
+            currentcart.AddToCart(album);
+            
+
+            return RedirectToAction("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(HttpContext);
+
+            ViewData["Cartsummary"] = cart.GetCount();
+
+            return PartialView();
+        }
+    }
+}
